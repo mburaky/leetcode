e7d17895c8dd16d8102278d86f1ac74d764ec64a
burak.yildirim <burak.yildirim@intra.local>
2019-04-10 10:23:29 +0300

    lengthOfLongestSubstring2

--- src/longestsubstringwithoutrepeatingcharacters/Solution.java
+++ src/longestsubstringwithoutrepeatingcharacters/Solution.java
@@ -3,20 +3,20 @@ package longestsubstringwithoutrepeatingcharacters;
 public class Solution {
 
     public int lengthOfLongestSubstring(String s) {
-
         int tempMax = 0;
-        int maxLength = 0 ;
+        int maxLength;
         for(int i = 0; i <= s.length()-1; i++){
+            maxLength=0;
             for(int j = i+1; j <= s.length(); j++){
                 char c = s.charAt(j-1);
                 String temp = s.substring(i,j);
                 if(temp.length() == 1){
                     maxLength = maxLength + 1;
-                    continue;
                 }else{
-                    if(!s.substring(i,j-1).contains(""+c)){
+                    String temp2 = s.substring(i,j-1);
+                    if(!temp2.contains(""+c)){
                         maxLength = maxLength + 1;
-                        if(maxLength == s.length()){
+                        if(maxLength > tempMax || maxLength == s.length()){
                             tempMax = maxLength;
                         }
                     }
@@ -24,7 +24,6 @@ public class Solution {
                         if(maxLength > tempMax) {
                             tempMax = maxLength;
                         }
-                        maxLength=0;
                         break;
                     }
                 }
@@ -33,7 +32,6 @@ public class Solution {
         if(s.length() == 1){
             return 1;
         }
-
         return tempMax;
     }
 }
